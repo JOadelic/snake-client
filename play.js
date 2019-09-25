1f9cfc210284460dbd330a395d8727191be9efaf
Your Name <you@example.com>
2019-09-25 19:17:07 +0000

    added handleUserInput function to be able to exit game on demand

--- play.js
+++ play.js
@@ -1,5 +1,27 @@
 // play.js
 const { connect } = require('./client');
 console.log('Connecting ...');
+
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+}
+
+const handleUserInput = function (data) {
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+}
+
 connect();
 
