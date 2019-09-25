a2bebfb7efbe3bd9546b7db0b0cfb2be8d0af7b0
Your Name <you@example.com>
2019-09-25 18:16:01 +0000

    completed

--- play.js
+++ play.js
@@ -1,22 +1,5 @@
-const net = require('net');
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '172.46.2.204',
-    port: 50541
-  });
-  
-  conn.on('connect', () => {
-    conn.write();
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8');
-
-
-  return conn;
-}
-
+// play.js
+const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
