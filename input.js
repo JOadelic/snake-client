abdcb31c2de6d81f06cdbad3dc57bc738db1448d
Your Name <you@example.com>
2019-09-25 20:31:43 +0000

    actiavted command buttons...peered with Avi

--- input.js
+++ input.js
@@ -1,17 +1,31 @@
+
+let connection;
+
 const setupInput = function() {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
+  stdin.on('data', handleUserInput);
   stdin.resume();
+  connection = conn;
   return stdin;
 }
 
 const handleUserInput = function (data) {
-  stdin.on('data', (key) => {
-    if (key === '\u0003') {
+  if (data === 'x') {
       process.exit();
-    }
-  });
-}
+  } else if (data === 'w') {
+    conn.write('Move: up');
+  } else if (data === 'a') {
+    conn.write('Move: left');
+  } else if (data === 's') {
+    conn.write('Move: down');
+  } else if (data === 'd') {
+    conn.write('Move: right');
+  }
+};
+
+
+
 
 module.exports = { setupInput };
